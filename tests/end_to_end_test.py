e3c8252509cf4aa304cb23b800d54fb241cc8eea
Alejandro & Alex C <prodev@unrulygroup.com>
2017-12-13 11:56:49 +0000

    Set a small sleep in the e2e test to allow the hungry person to receive everything needed.

--- tests/end_to_end_test.py
+++ tests/end_to_end_test.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 HUNGRY_PERSON_ENDPOINT = 'http://localhost:5000/v1'
 
@@ -10,6 +11,8 @@ def test_hungry_person_will_always_receive_exactly_what_they_asked_for():
     response = requests.post(HUNGRY_PERSON_ENDPOINT + '/order',
                              json={'name': 'Brownies', 'quantity': 2})
     assert response.text == 'OK'
+
+    time.sleep(3)
 
     baked_goods = requests.get(HUNGRY_PERSON_ENDPOINT + '/show-me-what-you-got')
 
